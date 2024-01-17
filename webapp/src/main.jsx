1e52142aaa02dba6101f9d1466cd2e42d948a3e4
Robert <marineau.robert@gmail.com>
2024-01-16 19:39:51 -0500

    Add async script for Google AdSense

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import './bootstrap-override.css'
 
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7785619856146899"
+     crossorigin="anonymous"></script>
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
