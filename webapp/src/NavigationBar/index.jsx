072b0fec24055dca979ff874a4dc7228898b0a66
Robert <marineau.robert@gmail.com>
2024-02-09 08:53:09 -0500

    Update Navbar brand name

--- webapp/src/NavigationBar/index.jsx
+++ webapp/src/NavigationBar/index.jsx
@@ -7,7 +7,7 @@ function NavigationBar() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="/">EHD Reshuffle</Navbar.Brand>
+                <Navbar.Brand href="/">EDH Reshuffle</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
